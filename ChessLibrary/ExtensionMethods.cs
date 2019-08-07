dc5ec64ac7a459c1f684e47cbf81cba6d5d12454
Youssef Victor <youssefvictor00@gmail.com>
2019-08-07 20:51:09 +0200

    Refactoring

---
+++ ChessLibrary/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using ChessLibrary.Pieces;
+
+namespace ChessLibrary
+{
+    public static class ExtensionMethods
+    {
+        public static bool Contains(this PawnMoveType moveType1, PawnMoveType moveType2) => (moveType1 & moveType2) == moveType2;
+    }
+
+
+}
---
+++ ChessLibrary/Pieces/PawnMoveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChessLibrary.Pieces
+{
+    [Flags]
+    public enum PawnMoveType
+    {
+        Invalid = 0,
+        OneStep = 1,
+        TwoSteps = 2,
+        Capture = 4,
+        Promotion = 8
+    }
+}
