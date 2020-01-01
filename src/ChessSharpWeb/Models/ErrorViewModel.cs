ad0809417865fc8aa070b8d700a5eb402c7247f4
Youssef Victor <youssefvictor00@gmail.com>
2020-01-01 11:45:41 +0200

    Initial commit to test build

---
+++ src/ChessSharpWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChessSharpWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
